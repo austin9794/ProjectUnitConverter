d6fb8ffc809566669086f8c5d3989678408f4db7
austin9794 <mohdumair72914@gmail.com>
2025-12-19 16:29:25 +0000

    temp logic

--- TemperatureConverter.java
+++ TemperatureConverter.java
@@ -1,3 +1,14 @@
-public class TemperatureConverter {
-    
+package unitconverter;
+
+public class TemperatureConverter implements Converter {
+
+    @Override
+    public double convert(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Celsius to Fahrenheit";
+    }
 }
