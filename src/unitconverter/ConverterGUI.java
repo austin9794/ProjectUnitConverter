1c6b9bae1b5bd4b5e9043940ad022dc40832d43c
austin9794 <mohdumair72914@gmail.com>
2025-12-20 16:35:46 +0000

    public class

--- src/unitconverter/ConverterGUI.java
+++ src/unitconverter/ConverterGUI.java
@@ -4,3 +4,11 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+public class ConverterGUI {
+
+    private JFrame frame;
+    private JTextField inputField;
+    private JLabel resultLabel;
+    private JComboBox<String> conversionBox;
+
+    
