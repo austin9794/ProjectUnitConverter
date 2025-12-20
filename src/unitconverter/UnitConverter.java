d79a4484a3ae516622009100c310da24d660ad96
austin9794 <mohdumair72914@gmail.com>
2025-12-20 16:19:52 +0000

    boolean logic

--- src/unitconverter/UnitConverter.java
+++ src/unitconverter/UnitConverter.java
@@ -5,25 +5,4 @@ import java.util.Scanner;
 public class UnitConverter {
 
     public static void run(Scanner scanner) {
-        System.out.println("Welcome to Unit Converter");
-
-        System.out.println("Choose conversion:");
-        System.out.println("1. Celsius to Fahrenheit");
-        System.out.println("2. Kilograms to Pounds");
-
-        int choice = scanner.nextInt();
-        System.out.print("Enter value: ");
-        double value = scanner.nextDouble();
-
-        Converter converter;
-
-        if (choice == 1) {
-            converter = new TemperatureConverter();
-        } else {
-            converter = new WeightConverter();
-        }
-
-        double result = converter.convert(value);
-        System.out.println("Result: " + result);
-    }
-}
+         boolean running = true;
