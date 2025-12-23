525e18b7258a4dc916f9930edc60bf7d26559c0f
austin9794 <mohdumair72914@gmail.com>
2025-12-23 12:45:41 +0000

    kilometers

--- src/unitconverter/LengthConverter.java
+++ src/unitconverter/LengthConverter.java
@@ -5,3 +5,7 @@ public class LengthConverter {
     public static double metersToKilometers(double m) {
         return m / 1000;
     }
+
+    public static double kilometersToMeters(double km) {
+        return km * 1000;
+    }
