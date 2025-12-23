6da2a0134bf35489ddb8161c52710f4bba1234cd
austin9794 <mohdumair72914@gmail.com>
2025-12-23 12:10:56 +0000

    JFrame

--- src/unitconverter/ConverterGUI.java
+++ src/unitconverter/ConverterGUI.java
@@ -3,4 +3,13 @@ package unitconverter;
 import javax.swing.*;
 import java.awt.*;
 
+public class ConverterGUI extends JFrame {
+
+    private JComboBox<String> categoryBox;
+    private JComboBox<String> conversionBox;
+    private JTextField inputField;
+    private JTextField outputField;
+
+    
+
 
