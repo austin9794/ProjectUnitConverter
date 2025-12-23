6c3448ca8bfaa829cdf66e0e63eea8db3055ef93
austin9794 <mohdumair72914@gmail.com>
2025-12-23 10:35:12 +0000

    new structure

--- src/unitconverter/UnitConverter.java
+++ src/unitconverter/UnitConverter.java
@@ -1,23 +1,7 @@
 package unitconverter;
 
-import java.util.Scanner;
-
 public class UnitConverter {
-
-    public static void run(Scanner scanner) {
-         boolean running = true;
-
-         while (running) {
-            System.out.println("\n=== Unit Converter ===");
-            System.out.println("1. Temperature Conversion");
-            System.out.println("2. Weight Conversion");
-            System.out.println("0. Exit");
-            System.out.print("Choose an option: ");
-
-             int choice = scanner.nextInt();
-
-            
-            }
-        
-        }
+    public static void main(String[] args) {
+        new ConverterGUI();
     }
+}
